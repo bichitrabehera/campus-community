9082df99ef2e4c435bc0d77c816756b749e0ae4c
Bichitra Behera <bichitrabehera.amcec@gmail.com>
2025-08-25 00:34:27 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,1 +1,1 @@
-export default function Dashboard(){ return <div className='prose'><h1>Welcome</h1><p>Select a section from the sidebar.</p></div> }
+export default function Dashboard(){ return <div className='prose'><h1>Welcomeee</h1><p>Select a section from the sidebar.</p></div> }
